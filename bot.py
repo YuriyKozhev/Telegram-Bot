bf772a7bb900493d1560cb83058f08365ef7403c
Юрий Кожев <yuakozhev@miem.hse.ru>
2020-04-19 21:11:40 +0300

    replace answer type: now bot returns current time

--- bot.py
+++ bot.py
@@ -30,21 +30,10 @@ class BotHandler:
             last_update = None
 
         return last_update
-
-
-# In[2]:
-
-
+    
 token = '1064687581:AAFrNcD-MWTOROzofaGHUBr0HcqANeAVuYE'
 
-
-# In[3]:
-
-
-greet_bot = BotHandler(token)  
-greetings = ('здравствуй', 'привет', 'ку', 'здорово')  
-now = datetime.datetime.now()
-
+bot = BotHandler(token)  
 
 def main():  
     new_offset = None
@@ -52,9 +41,9 @@ def main():
     hour = now.hour
 
     while True:
-        greet_bot.get_updates(new_offset)
+        bot.get_updates(new_offset)
 
-        last_update = greet_bot.get_last_update()
+        last_update = bot.get_last_update()
         
         if last_update is None: 
             continue
@@ -63,36 +52,12 @@ def main():
         last_chat_text = last_update['message']['text']
         last_chat_id = last_update['message']['chat']['id']
         last_chat_name = last_update['message']['chat']['first_name']
-
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-            today += 1
-
+        
+        bot.send_message(last_chat_id, datetime.datetime.now().strftime("%d-%b-%Y (%H:%M:%S)"))
         new_offset = last_update_id + 1
-
-
-# In[4]:
-
-
+        
 if __name__ == '__main__':  
     try:
         main()
     except KeyboardInterrupt:
         exit()
-
-
-
-
-# In[ ]:
-
-
-
-
